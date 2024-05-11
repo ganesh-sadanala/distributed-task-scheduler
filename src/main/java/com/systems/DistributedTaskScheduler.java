c9365dd59cfa0fef6fd407cf38a044c1d88c60b2
gsadanala <ganesh8374387106@gmail.com>
2024-05-11 00:45:04 -0400

    test springboot server to redis cluster connection

--- src/main/java/com/systems/DistributedTaskScheduler.java
+++ src/main/java/com/systems/DistributedTaskScheduler.java
@@ -1,12 +1,32 @@
 package com.systems;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.util.Properties;
 
 @SpringBootApplication
-public class DistributedTaskScheduler {
+public class DistributedTaskScheduler implements CommandLineRunner {
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(DistributedTaskScheduler.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try (RedisConnection connection = connectionFactory.getConnection()) {
+            // Get the server info
+            Properties serverInfo = connection.info();
+            System.out.println("Connected to Redis cluster successfully!");
+        } catch (Exception e) {
+            System.out.println("Failed to connect to Redis cluster: " + e.getMessage());
+        }
     }
 }
